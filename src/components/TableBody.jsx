c65263bd38fdddc43712984a84f3c2781519c1a1
Michelle Faure <faure.s.michelle@gmail.com>
2024-08-02 16:00:33 -0400

    change text color

--- src/components/TableBody.jsx
+++ src/components/TableBody.jsx
@@ -3,31 +3,31 @@ const TableBody = ({ currentItems, handleRowClick }) => {
     <tbody className="divide-y divide-white text-white font-semibold">
       {currentItems.map((car, index) => (
         <tr key={index} onClick={() => handleRowClick(car)}>
-          <td className="whitespace-wrap pe-4 py-2 text-gray-700 max-w-28">
+          <td className="whitespace-wrap pe-4 py-2 text-white max-w-28 ">
             {car.class}
           </td>
-          <td className="whitespace-wrap pe-4  py-2 text-gray-700">
+          <td className="whitespace-wrap pe-4  py-2 text-white">
             {car.fuel_type}
           </td>
-          <td className="whitespace-wrap pe-4  py-2 text-gray-700">
+          <td className="whitespace-wrap pe-4  py-2 text-white">
             {car.make}
           </td>
-          <td className="whitespace-wrap pe-4 py-2 text-gray-700 max-w-28">
+          <td className="whitespace-wrap pe-4 py-2 text-white max-w-28">
             {car.model}
           </td>
-          <td className="whitespace-wrap pe-4 py-2 text-gray-700">
+          <td className="whitespace-wrap pe-4 py-2 text-white">
             {car.year}
           </td>
-          <td className="whitespace-wrap pe-4 py-2 text-center text-gray-700">
+          <td className="whitespace-wrap pe-4 py-2 text-center text-white">
             {car.transmission}
           </td>
-          <td className="whitespace-wrap pe-4 py-2 text-center text-gray-700">
+          <td className="whitespace-wrap pe-4 py-2 text-center text-white">
             {car.city_mpg}
           </td>
-          <td className="whitespace-wrap pe-4 py-2 text-center text-gray-700">
+          <td className="whitespace-wrap pe-4 py-2 text-center text-white">
             {car.highway_mpg}
           </td>
-          <td className="whitespace-wrap pe-4 py-2 text-center text-gray-700">
+          <td className="whitespace-wrap pe-4 py-2 text-center text-white">
             {car.combination_mpg}
           </td>
         </tr>
